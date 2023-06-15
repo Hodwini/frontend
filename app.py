37bd3b28d941a5f2d882ea6e16f2d132bed6b6c3
Nikita Simakin <tanukofisimakin@gmail.com>
2023-06-15 04:35:41 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/home')
+@app.route('/')
+def page_index():
+    return render_template("index.html")
+
+@app.route('/wiki')
+@app.route('/')
+def page_wiki():
+    return render_template("wiki.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
